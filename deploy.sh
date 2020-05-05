0693f172b796473d597d44f87a845c11d7bc2bcd
Ruben van den Hout <ruben@MacBook-Pro.local>
2020-05-05 23:31:18 +0200

    typo fixes

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
-echo 'Buildling static webpage...'
-gssg --url 'http://fransruben.io' --dest 'deploy'
+# echo 'Buildling static webpage...'
+# gssg --url 'http://fransruben.io' --dest 'deploy'
 
-echo 'Moving deploy files...'
-cp -r deploy/ ./
-rm -R -f deploy
+# echo 'Moving deploy files...'
+# cp -r deploy/ ./
+# rm -R -f deploy
 
-echo 'Commit and push changes...'
-git add .
-git commit -a -m "deploy commit"
+# echo 'Commit and push changes...'
+# git add .
+read -p "Commit message: " commit_message
+echo $commit_message
+git commit -a -m "$commit_message"
 git push
