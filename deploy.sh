9c0f26b64555caad2a16c32054724b1f66794715
Ruben van den Hout <ruben@MacBook-Pro.local>
2020-05-05 17:18:00 +0200

    deploy commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
+echo 'Buildling static webpage...'
 gssg --url 'http://fransruben.io' --dest 'deploy'
+
+echo 'Moving deploy files...'
+cp -r deploy/ ./
+rm -R -f deploy
+
+echo 'Commit and push changes...'
 git add .
-git commit -a -m "commit" (do not need commit message either)
+git commit -a -m "deploy commit"
 git push
--- deploy/assets/js/masonry.js
+++
@@ -1,7 +0,0 @@
-var msnry = new Masonry( '.gh-gallery-container', {
-    columnWidth: '.gh-gallery-sizer',
-    itemSelector: '.gh-gallery-item',
-    transitionDuration: 0,
-    percentPosition: true,
-    gutter: '.gh-gallery-gutter'
-})
