adeff8187698ea86b9914082e7380ec67cd1c502
Ruben van den Hout <ruben@MacBook-Pro.local>
2020-05-08 12:38:30 +0200

    Post order fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-# echo 'Buildling static webpage...'
-# gssg --url 'http://fransruben.io' --dest 'deploy'
+echo 'Buildling static webpage...'
+gssg --url 'http://fransruben.io' --dest 'deploy'
 
-# echo 'Moving deploy files...'
-# cp -r deploy/ ./
-# rm -R -f deploy
+echo 'Moving deploy files...'
+cp -r deploy/ ./
+rm -R -f deploy
 
-# echo 'Commit and push changes...'
-# git add .
+echo 'Commit and push changes...'
+git add .
 read -p "Commit message: " commit_message
 echo $commit_message
 git commit -a -m "$commit_message"
